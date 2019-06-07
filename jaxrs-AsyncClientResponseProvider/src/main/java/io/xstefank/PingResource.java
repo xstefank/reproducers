4bddc1b8351aa4a1bdf5a09f6edb14b05b1000af
xstefank <xstefank122@gmail.com>
2019-06-07 11:33:54 +0200

    Add jaxrs-AsyncClientResponseProvider reproducer

---
+++ jaxrs-AsyncClientResponseProvider/src/main/java/io/xstefank/PingResource.java
@@ -0,0 +1,36 @@
+package io.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+@Path("/")
+public class PingResource {
+
+    @Context
+    private UriInfo uriInfo;
+    
+    @GET
+    @Path("async")
+    public String async() {
+        Client client = ClientBuilder.newClient();
+        client.register(DummyAsyncClientResponseProvider.class);
+
+        Response response = client.target(uriInfo.getBaseUriBuilder().path("async-client").build())
+            .request().get();
+
+        return response.readEntity(String.class);
+    }
+
+    @GET
+    @Path("async-client")
+    public CompletionStage<String> asyncClient() {
+        return CompletableFuture.supplyAsync(() -> "original string");
+    }
+}
